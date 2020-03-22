594cf7e71e80be99a286e20abc1b9408b7840482
brilyanrhp <619mistico@gmail.com>
2020-03-22 15:16:58 +0700

    fix dikit

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from twitter import Twitter
 import time
 
+#test pancing ke heroku supaya keluar dyno
+#test kedua pancing worker supaya muncul...
+
 tw = Twitter()
 
 def start():
-    print("memulai program")
+    print("Starting program...")
     dms = list()
     while True:
         if len(dms) != 0:
@@ -12,33 +15,34 @@ def start():
                 message = dms[i]['message']
                 sender_id = dms[i]['sender_id']
                 id = dms[i]['id']
-                if len(message) != 0 and len(message) < 200:
+
+                if len(message) != 0 and len(message) < 280:
                     if "-blorabase" in message:
                         message = message.replace("-blorabase", "")
                         if len(message) != 0:
                             if dms[i]['media'] == None:
-                                print("DM akan di post")
+                                print("DM will be posted")
                                 tw.post_tweet(message)
                                 tw.delete_dm(id)
                             else:
-                                print("DM akan di post dengan media")
-                                tw.post_tweet_with_media(message, dms[i]['media'])
+                                print("DM will be posted with media")
+                                print(dms[i]['shorted_media_url'])
+                                tw.post_tweet_with_media(message, dms[i]['media'],dms[i]['shorted_media_url'], dms[i]['type'])
                                 tw.delete_dm(id)
-
                         else:
-                            print("DM akan dihapus karena kosong")
+                            print("DM deleted because its empty..")
                             tw.delete_dm(id)
-                            #hapus dm
                     else:
-                        print("DM akan dihapus karena tidak ada keyword")
+                        print("DM will be deleted because does not contains keyword..")
                         tw.delete_dm(id)
-                        #hapus dm pancingan
 
             dms = list()
+
         else:
-            print("DM kosong")
+            print("Direct message is empty...")
             dms = tw.read_dm()
             if len(dms) == 0:
-                time.sleep(30)
+                time.sleep(60)
+
 if __name__ == "__main__":
     start()
